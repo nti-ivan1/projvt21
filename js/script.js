0536df5b8f3fc399a61d06b29f6670e5f724e7b9
nti-ivan1 <71006110+nti-ivan1@users.noreply.github.com>
2021-03-31 11:18:08 +0200

    github fixat

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Denna fil skall innehålla JavaScript (JS)
+const nav = document.querySelector('.navLinks')
+const  meny = document.querySelector('.meny')
+
+function toggleMeny() {
+    nav.classList.toggle('navActive')
+    meny.classList.toggle('toggle')
+}
+
+meny.addEventListener('click', toggleMeny)
